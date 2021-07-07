9fbb2e3ca54e95519c2af87d1d4cfd9037cd9207
Verbena <veve292@live.it>
2021-07-07 12:57:19 +0100

    added books

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("JavaScript loaded");
-});
+  const handleFormSubmit = function (event) {
+    event.preventDefault();
 
-const books = [];
+    const bookList = document.querySelector("#reading-list");
+
+    const newBook = document.createElement("li");
+    newBook.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
+    bookList.appendChild(newBook);
+
+    form.reset();
+  };
+
+  const form = document.querySelector("#new-item-form");
+  form.addEventListener("submit", handleFormSubmit);
+  const newButton = document.createElement("button");
+  newButton.textContent = "Delete";
+  const body = document.querySelector("body");
+  body.appendChild(newButton);
+
+  const handleDeleteSubmit = function (event) {};
+});
